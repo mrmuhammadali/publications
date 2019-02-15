fc94474f300e5b58b37909f0dcf94c21f9c8e9f6
Muhammad Ali <mr.muhammad.ali@live.com>
2019-02-15 19:39:31 +0500

    filters data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,31 @@ import ReactTooltip from 'react-tooltip'
 
 // src
 import ForceDirectedGraph from './ForceDirectedGraph'
-import data from './data/sample.json'
+import linksData from './data/links.json'
+import nodesData from './data/nodes.json'
+
+const filterData = data => {
+  const nodes = data.nodes.reduce((final, node) => {
+    return {
+      ...final,
+      [node.id]: node,
+    }
+  }, {})
+  const filteredLinks = data.links.filter(({ source, target }) => {
+    const isSourceCorrect = Boolean(nodes[source])
+    const isTargetCorrect = Boolean(nodes[target])
+
+    return isSourceCorrect && isTargetCorrect
+  })
+  const filteredNodes = filteredLinks.reduce((final, { source, target }) => {
+    return [...final, nodes[source], nodes[target]]
+  }, [])
+
+  return { links: filteredLinks, nodes: filteredNodes }
+}
+
+const data = { nodes: nodesData, links: linksData }
+const filteredData = filterData(data)
 
 export default class ForceDirectedExample extends React.Component {
   state = {
@@ -13,6 +37,8 @@ export default class ForceDirectedExample extends React.Component {
 
   render() {
     const { strength } = this.state
+
+    console.log(filteredData, linksData.length)
 
     return (
       <div className="force-directed-example">
@@ -23,9 +49,9 @@ export default class ForceDirectedExample extends React.Component {
           {' Reweight '}
         </button>
         <ForceDirectedGraph
-          data={data}
-          height={500}
-          width={500}
+          data={filteredData}
+          height={window.innerHeight}
+          width={window.innerHeight}
           animation
           strength={strength}
         />
